b5a9724eb2c1e3cf8a397c4c5b1c4ad8e3c4e78b
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-04-03 10:58:27 +0800

    add comments and usage

--- 02-face-recognition-built-in/face_recognition.py
+++ 02-face-recognition-built-in/face_recognition.py
@@ -1,38 +1,53 @@
+# Usage: python face_recognition.py --image="./resources/images/faces/val/elton_john/1.jpg"
+
+# Imports necessary libraries
 import numpy as np
 import cv2 as cv
 import argparse
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
+# Add an argument to the parser that specifies the path to the input image
 ap.add_argument("-i", "--image", required=True,
     help="path to input image")
 
 args = vars(ap.parse_args())
 image = cv.imread(args["image"])
-cv.imshow('est', image)
+cv.imshow('Test', image)
 
+# Load a pre-trained Haar cascade classifier for detecting faces in images
 haar_cascade = cv.CascadeClassifier('./resources/data/haar_face.xml')
+# Define a list of people's names that correspond to different labels
+# that will be output by the face recognition algorithm.
 people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
 
 # features = np.load('features.npy', allow_pickle=True)
 # labels = np.load('label.npy')
 
+# Create a Local Binary Patterns Histograms (LBPH) face recognizer object
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
+# Load a pre-trained face recognizer from a YAML file
 face_recognizer.read('face_trained.yml')
 
+# Convert the input image to grayscale using the 'cvtColor' function
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 cv.imshow('Person', gray)
 
-# Detect the face in image
+# Detect faces in the grayscale image using the Haar cascade classifier.
 faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
 
 for (x, y, w, h) in faces_rect:
+    # Extract the region of interest (ROI) in the grayscale image that corresponds to the detected face
     faces_roi = gray[y:y+h, x:x+w]
     
+    # Use the face recognizer to predict the label (person's name) and confidence score for the ROI
     label, confidence = face_recognizer.predict(faces_roi)
     print(f'Label = {people[label]} with confidence of {confidence}')
     
+    # Output the predicted label on the input image using the 'putText' function
     cv.putText(image, str(people[label]), (20, 20), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), thickness=2)
+    
+    # Draw a rectangle around the detected face on the input image using the 'rectangle' function
     cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
     
 cv.imshow('Detected face', image)
--- 02-face-recognition-built-in/index.py
+++ 02-face-recognition-built-in/index.py
@@ -1,8 +1,15 @@
+# The purpose of this code is to train a face recognition model using the 
+# LBPH algorithm on a set of images
+# Usage: python index.py 
+
+# Imports necessary libraries
 import os 
 import cv2 as cv
 import numpy as np
 
+# List of people names
 people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+# Path to the directory where training images are located   
 DIR = './resources/images/faces/train'
 # Create a Haar cascade classifier for face detection
 haar_cascade = cv.CascadeClassifier('./resources/data/haar_face.xml')
@@ -10,45 +17,64 @@ p = []
 
 for i in os.listdir('./resources/images/faces/train'):
     p.append(i)
-    
-print(DIR)
 
+# Two empty lists to store image features and their corresponding labels
 features = []
 labels = []
 
+# Function to read training images and their labels
 def create_train():
+    # Loop through each person's name in 'people' list
     for person in people:
+        # Construct the path to the directory where images of every person are located
         path = os.path.join(DIR, person)
+        # Assign a unique integer label to each person in 'people' list
         label = people.index(person)
         
+        # Loop through each image file in the current person's directory
         for img in os.listdir(path):
+            # Construct the path to the image file
             img_path = os.path.join(path, img)
             
+            # Read the image file in grayscale
             img_array = cv.imread(img_path)
             gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
             
+            # Detect faces in the image
             faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
             
             # Draw a rectangle around each detected face in the original color image
             for (x, y, w, h) in faces_rect:
+                # Extract the region of interest (ROI) that contains the face
                 faces_roi = gray[y:y+h, x:x+w]
+                # Append the ROI to the features list
                 features.append(faces_roi)
+                # Append the label of the person to the labels list
                 labels.append(label)
                 
 create_train()
 
 print('Training done -------------')
 
+# Convert features and labels lists to numpy arrays
 features = np.array(features, dtype='object')
 labels = np.array(labels)
 
+# Create an instance of LBPH face recognizer
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 
 # Train the Recognizer on the features list and the labels list
 face_recognizer.train(features, labels)
+
+# Save the trained recognizer to disk
 face_recognizer.save('face_trained.yml')
+
+# Save the features and labels lists to disk
 np.save('features.npy', features)
 np.save('labels.npy', labels)
 
 # print(f'Length of the features = {len(features)}')
 # print(f'Length of the labels = {len(labels)}')
+
+# Note: 
+# Region of Interest - ROI helps to focus on a specific part (e.g. Face) of an image for further analysis or processing
